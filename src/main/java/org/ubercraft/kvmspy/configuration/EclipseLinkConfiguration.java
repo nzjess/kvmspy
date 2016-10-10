b514c886266c222f0d4a622cf499976b8a0b90d7
Jesse McLaughlin <jesse@loveholidays.com>
2016-10-10 10:57:49 +0100

    use eclipselink instead of hibernate

---
+++ src/main/java/org/ubercraft/kvmspy/configuration/EclipseLinkConfiguration.java
@@ -0,0 +1,32 @@
+package org.ubercraft.kvmspy.configuration;
+
+import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+@Configuration
+public class EclipseLinkConfiguration extends JpaBaseConfiguration {
+
+    protected EclipseLinkConfiguration(DataSource dataSource, JpaProperties properties,
+                                       ObjectProvider<JtaTransactionManager> jtaTransactionManagerProvider) {
+        super(dataSource, properties, jtaTransactionManagerProvider);
+    }
+
+    @Override
+    protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+        return new EclipseLinkJpaVendorAdapter();
+    }
+
+    @Override
+    protected Map<String, Object> getVendorProperties() {
+        return Collections.singletonMap("eclipselink.weaving", "false");
+    }
+}
